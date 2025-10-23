b7ef0bf4117136e7888c95b3dda757124eed1026
“24caratgoldenlabububobatea” <jonathanrasmussen2020@gmail.com>
2025-10-23 13:04:08 +0200

    it was being annoying so i got rid of it

--- data.py
+++
@@ -1,8 +0,0 @@
-regular_meals = ["Pizza", "Chicken Nuggets", "Spaghetti"]
-
-weekly_specials = [
-    ["Tacos", "Chili", "Burritos"],
-    ["Hamburgers", "Hot Dogs", "Fries"],
-    ["Fish Sticks", "Mac and Cheese", "Salad"],
-    ["Meatloaf", "Mashed Potatoes", "Corn"]
-]
