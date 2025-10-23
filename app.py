bd552e38d0cef3ae770cb6cf5db98e3c57d3d3d9
“24caratgoldenlabububobatea” <jonathanrasmussen2020@gmail.com>
2025-10-23 10:21:38 +0200

    I added the data to app.py then i made it so everytime you refreash the tab it tell you the different meals for the month.

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
+
+regular_meals = ["Pizza", "Chicken Nuggets", "Spaghetti"]
+
+weekly_specials = [
+    ["Tacos", "Chili", "Burritos"],
+    ["Hamburgers", "Hot Dogs", "Fries"],
+    ["Fish Sticks", "Mac and Cheese", "Salad"],
+    ["Meatloaf", "Mashed Potatoes", "Corn"]
+]
 
 @app.route('/')
 def home():
@@ -8,7 +18,9 @@ def home():
 
 @app.route('/meny')
 def meny():
-    return render_template("meny.html")
+    current_week = random.randint(1, 4)
+    meals_this_week = regular_meals + weekly_specials[current_week - 1]
+    return render_template("meny.html", meals=meals_this_week, week=current_week)
 
 @app.route('/varer')
 def varer():
