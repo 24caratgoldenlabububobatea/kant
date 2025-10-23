fd25d690024fb06ff3cab55e5451136af3c6d001
“24caratgoldenlabububobatea” <jonathanrasmussen2020@gmail.com>
2025-10-23 10:07:47 +0200

    Removed a redundant page on my site and added data to it. i tried doing this on windows but it didn't want to work.

---
+++ data.py
@@ -0,0 +1,8 @@
+regular_meals = ["Pizza", "Chicken Nuggets", "Spaghetti"]
+
+weekly_specials = [
+    ["Tacos", "Chili", "Burritos"],
+    ["Hamburgers", "Hot Dogs", "Fries"],
+    ["Fish Sticks", "Mac and Cheese", "Salad"],
+    ["Meatloaf", "Mashed Potatoes", "Corn"]
+]
